fa1cb9119b8c6fa2425a5ecc49773ca1daaefe44
GiovyUrbina <alejoub1998@gmail.com>
2021-07-19 08:15:33 -0500

    Primer commit

---
+++ src/main/java/com/FundamentosN/FundamentosN/FundamentosNApplication.java
@@ -0,0 +1,13 @@
+package com.FundamentosN.FundamentosN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosNApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FundamentosN/FundamentosN/FundamentosNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FundamentosN.FundamentosN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
